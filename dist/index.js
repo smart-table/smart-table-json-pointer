d017394ee3f950e93d5d18a1529363c86850497e
Lorenzofox3 <laurent34azerty@gmail.com>
2017-02-01 11:54:29 -0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,41 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global['smart-table-operators'] = factory());
+}(this, (function () { 'use strict';
+
+function pointer (path) {
+
+  const parts = path.split('.');
+
+  function partial (obj = {}, parts = []) {
+    const p = parts.shift();
+    const current = obj[p];
+    return (current === undefined || parts.length === 0) ?
+      current : partial(current, parts);
+  }
+
+  function set (target, newTree) {
+    let current = target;
+    const [leaf, ...intermediate] = parts.reverse();
+    for (let key of intermediate.reverse()) {
+      if (current[key] === undefined) {
+        current[key] = {};
+        current = current[key];
+      }
+    }
+    current[leaf] = Object.assign(current[leaf] || {}, newTree);
+    return target;
+  }
+
+  return {
+    get(target){
+      return partial(target, [...parts])
+    },
+    set
+  }
+}
+
+return pointer;
+
+})));
---
+++ test/index.js
@@ -0,0 +1,38 @@
+import zora from 'zora';
+import pointer from '../index';
+
+export default zora()
+  .test('pointer get', function * (t) {
+    const {get:foo} = pointer('foo');
+    t.equal(foo(), undefined);
+    t.equal(foo('foo'), undefined);
+    t.equal(foo({bar: 'foo'}), undefined);
+    t.equal(foo({foo: 'bar'}), 'bar');
+    const {get:foobar} = pointer('foo.bar');
+    t.equal(foobar(), undefined);
+    t.equal(foobar('blah'), undefined);
+    t.equal(foobar({foo: 'bar'}), undefined);
+    t.equal(foobar({foo: {bar: 'woot'}}), 'woot');
+    const {get:foobarblah} = pointer('foo.bar.blah');
+    t.equal(foobarblah({foo: {bar: {blah: 'hello'}}}), 'hello');
+  })
+  .test('pointer set short path', function * (t) {
+    const {set:foo} = pointer('foo');
+    const target = {};
+    foo(target, {otherfoo: 'bar'});
+    t.deepEqual(target, {foo: {otherfoo: 'bar'}});
+  })
+  .test('pointer set long path', function * (t) {
+    const {set:foobar} = pointer('foo.bar');
+    const target = {};
+    foobar(target, {blah: 'woot'});
+    t.deepEqual({foo: {bar: {blah: 'woot'}}}, target);
+  })
+  .test('pointer should blend if there is already a tree', function * (t) {
+    const {set:foo} = pointer('foo');
+    const target = {foo: {bar: 'woot'}};
+    foo(target, {anotherblah: 'wootbis'});
+    t.deepEqual(target, {foo: {bar: 'woot', anotherblah: 'wootbis'}});
+  })
+  .run();
+
